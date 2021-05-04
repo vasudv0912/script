b2404500c34fbc31e3119f65714ac7e4dd663b12
vasudv0912 <vasudv0912@gmail.com>
2021-05-04 22:54:12 +0530

    script new

--- abc.py
+++ abc.py
@@ -1,31 +1,29 @@
 import faster_than_requests as requests
-import datetime
 import json
 import smtplib
 import time
 from email.mime.text import MIMEText
 server=smtplib.SMTP_SSL("smtp.gmail.com" , 465)
-server.login("vasudv0912@gmail.com","vasu9@0609")
 currenttitle0=""
-count=0
 print("Script started...")
 while True:
 	try: 
-		x=datetime.datetime.now()
 		d = requests.get2dict("https://www.binance.com/gateway-api/v3/public/market/notice/get?page=1&rows=1")
-		count+=1
-		print(count)
 		currenttitle1=(json.loads(d[3]['data'])[0]['title'])
 		publishtimestramp=(json.loads(d[3]['data'])[0]['time'])
 		if currenttitle0!=currenttitle1:
 			currenttitle0=currenttitle1
-			msg = MIMEText("Title:- "+str(currenttitle0)+" // Publish Time:- "+str(publishtimestramp)+"// Current Time:-"+str(datetime.datetime.now()))
-			msg['Subject'] = "Binance Update: " 
+			print(currenttitle0)
+			msg = MIMEText("Title:- "+str(currenttitle0)+" // Publish Time:- "+str(publishtimestramp)+"// Current Time:-"+ str(time.time()*1000))
+			msg['Subject'] = "Binance Update: " + str(currenttitle0)
 			msg['From'] = 'vasudv0912@gmail.com'
 			recipients= ['jsmith503@gmail.com','singhakash414@gmail.com']
 			msg['To'] = ", ".join(recipients)
+			server.login("vasudv0912@gmail.com","vasu9@0609")
 			server.sendmail('vasudv0912@gmail.com',recipients, msg.as_string())
+			server.quit()
 	except Exception as e: 
 		print(str(e)+" will try after 3 sec")
-		time.sleep(3)  
-		continue
+		server.quit()
+		server.login("vasudv0912@gmail.com","vasu9@0609")
+		time.sleep(3)
