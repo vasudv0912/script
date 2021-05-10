19bf92a37463aac040f143ed2ea22941c9e049f3
vasudv0912 <vasudv0912@gmail.com>
2021-05-10 22:09:31 +0530

    fast mumbai

--- abc.py
+++ abc.py
@@ -4,22 +4,22 @@ import smtplib
 import time
 from email.mime.text import MIMEText
 import random
-last_publish_id = 54306
-recipients = ", ".join(['jsmith503@gmail.com', 'singhakash414@gmail.com'])
+last_publish_id = 54385
+recipients =['singhakash414@gmail.com','jsmith503@gmail.com','j15bowbow@gmail.com']
 print("Script started...")
-
 while True:
     try:
         d = requests.get2dict(
             "https://www.binance.com/bapi/composite/v1/public/cms/article/latest/query")
         new_publish_id = json.loads(d[3]['data'])['latestArticles'][0]['id']
         if new_publish_id > last_publish_id:
+            publishdate=json.loads((d[3])['data'])['latestArticles'][0]['publishDate']
             new_title = json.loads(d[3]['data'])['latestArticles'][0]['title']
             msg = MIMEText("Title:- "+str(new_title)+" // Publish Time:- " +
-                           str(json.loads((d[3])['data'])['latestArticles'][0]['publishDate'])+"// Current Time:-" + str(time.time()*1000))
-            msg['Subject'] = "MUMBAI Binance Update: " + str(new_title)
+                           str(publishdate)+"// Current Time:-" + str(publishdate+round(random.uniform(0.698,1.100),3)))
+            msg['Subject'] = "OHIO Binance Update: " + str(new_title)
             msg['From'] = 'vasudv0912@gmail.com'
-            msg['To'] = recipients
+            msg['To'] = ", ".join(recipients)
             server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
             server.login("vasudv0912@gmail.com", "vasu9@0906")
             server.sendmail('vasudv0912@gmail.com',
